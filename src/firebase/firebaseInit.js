73b9034cdd819835d678924f182e8ee74e6acbde
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-04-10 16:22:58 +0300

    initial commit

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -4,12 +4,12 @@ import "firebase/storage"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDT4-5LetHVVg8gEqXHFmRS8pUPNV-CGiU",
-  authDomain: "blue-economy-c98d6.firebaseapp.com",
-  projectId: "blue-economy-c98d6",
-  storageBucket: "blue-economy-c98d6.appspot.com",
-  messagingSenderId: "612934762026",
-  appId: "1:612934762026:web:f77bf77c82c6aa64454d53",
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
